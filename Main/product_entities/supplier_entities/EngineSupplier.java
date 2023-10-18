5164c16655fdab2e16201026a58cd278629c96f2
Joao Freitas <joaovitor6586@gmail.com>
2023-10-18 16:09:53 -0300

    Changed Engine Supplier

--- Main/product_entities/supplier_entities/EngineSupplier.java
+++ Main/product_entities/supplier_entities/EngineSupplier.java
@@ -1,4 +1,18 @@
 package Main.product_entities.supplier_entities;
+package Main.product_entities.components_entities;
 
 public class EngineSupplier {
+
+    Component engine = new Engine();
+
+    public void makeEngine(int number){
+
+    }
+
+    public static void main(String[] args){
+
+
+    }
+
+
 }
