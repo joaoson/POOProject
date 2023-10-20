810da5b11624909a8e6e2d67f96b37c3fca40775
Your Name <felipepoulos@gmail.com>
2023-10-19 21:13:35 -0300

    Created sport Class

--- src/product_entities/Sport.java
+++ src/product_entities/Sport.java
@@ -1,4 +1,19 @@
 package product_entities;
 
 public class Sport extends Car{
+    private static double finalPrice;
+    private String name = "Sport";
+    private double weight = 975;
+
+    public Sport(double price){
+        this.finalPrice = price;
+        this.price = price;
+        this.chassi = random.nextInt(0,10000);
+    }
+
+    public Sport(){
+        this.price = finalPrice;
+        this.chassi = random.nextInt(0,10000);
+    }
+
 }
