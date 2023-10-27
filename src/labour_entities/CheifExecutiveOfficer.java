92910cc59c7fda7d87d0e43a1fefdf776def6d4d
Your Name <felipepoulos@gmail.com>
2023-10-26 21:26:20 -0300

    Last fixes made.

--- src/labour_entities/CheifExecutiveOfficer.java
+++
@@ -1,9 +0,0 @@
-package labour_entities;
-
-import java.util.Calendar;
-
-public class CheifExecutiveOfficer extends Manager{
-    public CheifExecutiveOfficer(String name, String password, String login, String dateOfBirth, String gender) {
-        super(name, password, login, dateOfBirth, gender);
-    }
-}
---
+++ src/labour_entities/ChiefExecutiveOfficer.java
@@ -0,0 +1,7 @@
+package labour_entities;
+
+public class ChiefExecutiveOfficer extends Manager{
+    public ChiefExecutiveOfficer(String name, String password, String login, String dateOfBirth, String gender) {
+        super(name, password, login, dateOfBirth, gender);
+    }
+}
